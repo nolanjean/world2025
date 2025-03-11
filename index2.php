1ed42e15ee7d74b7d7f3739a70e195cb68607118
nolanjean <nobodyplayeur@gmail.com>
2025-03-11 16:05:04 +0100

    Deuxième commit, changement

--- index2.php
+++ index2.php
@@ -33,42 +33,17 @@ $desPays = getCountriesByContinent($continent);
        <?php
        // $desPays est un tableau dont les éléments sont des objets représentant
        // des caractéristiques d'un pays (en relation avec les colonnes de la table Country)
-          $pays = $desPays[0]; ?>
+          //$pays = $desPays[0]; 
+          foreach($desPays as $pays){
+          ?>
           <tr>
             <td> <?php echo $pays->Name ?></td>
             <td> <?php echo $pays->Population ?></td>
           </tr>
+          <?php
+          }?>
      </table>
     </div>
-    <p>
-        <code>
-      <?php
-        var_dump($desPays[0]);
-        ?>
-        </code>
-    </p>
-    <section class="jumbotron">
-      <div class="container">
-        <h1 class="jumbotron-heading">Tableau d'objets</h1>
-        <p>Le contenu ci-dessus représente une vue "debug" du premier élément d'un tableau. Ce tableau est
-          constitué d'objets PHP "standard" (stdClass).</p>
-        <p>Pour accéder à l'<b>attribut</b> d'un <b>objet</b> on utilisera le symbole <b><code>-></code></b>.
-          Ainsi, pour accéder à l'attribut <code>Name</code> du premier pays de la liste
-          <code>$desPays</code> on fera <b><code>$desPays[0]->Name</code></b>
-        </p>
-        <p>La variable <b><code>$desPays</code></b> référence un tableau (<i>array</i>).
-          Pour générer le code HTML (table), vous devrez coder une boucle,
-          par exemple de type <b><code>foreach</code></b> sur l'ensembles des objets de ce tableau. </p>
-        <p>Référez-vous à la structure des tables SQL pour connaître le nom des <b><code>attributs</code></b>.
-          En effet, les objets du tableau ont pour attributs les noms des colonnes de la table interrogée par un requête SQL, via l'appel à la
-          fonction <b><code>getCountriesByContinent</code></b> (du script <b><code>manager-db.php</code></b>.</p>
-        <p>Par exemple <b><code>Name</code></b> est une des colonnes de la table <b><code>Country</code></b> de la base de données.</p>
-          <p> Bonne programmation</p>
-          <div class="alert alert-warning" role="alert">
-            Cette section ne s'auto-détruit pas automatiquement, ce sera à vous de le faire, une fois compris son message !
-          </div>
-      </div>
-    </section>
   </div>
 </main>
 
